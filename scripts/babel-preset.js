28cbb0fe7d721d2f523ce8f29daa79238724ea15
Aadarsh131 <aadarshkumar131@gmail.com>
2024-12-21 01:46:42 +0530

    updated babel-presets

--- scripts/babel-preset.js
+++ scripts/babel-preset.js
@@ -2,7 +2,7 @@ const BABEL_ENV = process.env.BABEL_ENV;
 const isCommonJS = BABEL_ENV !== undefined && BABEL_ENV === 'cjs';
 const isESM = BABEL_ENV !== undefined && BABEL_ENV === 'esm';
 
-export default () => ({
+module.exports = () => ({
   presets: [
     ['@babel/preset-typescript'],
     [
